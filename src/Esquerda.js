6937f6eb9debe1cdda79999e34e3ba615a91c956
pablodamascenoo <pablofacex@gmail.com>
2022-03-11 20:56:04 -0300

    feat: add map on Posts

--- src/Esquerda.js
+++ src/Esquerda.js
@@ -1,26 +1,11 @@
 import Stories from "./Stories";
-import Post from "./Post";
+import Posts from "./Posts";
 
 export default function Esquerda() {
   return (
     <div className="esquerda">
       <Stories />
-      <div className="posts">
-        <Post
-          profile="assets/img/meowed.svg"
-          photo="assets/img/gato-telefone.svg"
-          curtidasPhoto="assets/img/respondeai.svg"
-          likes="150.254"
-          likePerson="respondeAi"
-        />
-        <Post
-          profile="assets/img/barked.svg"
-          photo="assets/img/dog.svg"
-          curtidasPhoto="assets/img/adorable_animals.svg"
-          likes="99.159"
-          likePerson="respondeAi"
-        />
-      </div>
+      <Posts />
     </div>
   );
 }
